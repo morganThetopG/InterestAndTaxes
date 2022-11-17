740f41eca2f2d6c0d295ea319bf7df63dd2e1602
morganThetopG <117848844+morganThetopG@users.noreply.github.com>
2022-11-17 09:33:01 -0700

    Update Taxes!.py

--- Taxes!.py
+++ Taxes!.py
@@ -3,7 +3,7 @@ un=float(input("enter a number: "))
 print("\n The Number You Entered Will Enter The Intrest Simulator \n")
 I=0
 while True:
-    sleep(1)
+    sleep(0.3)
     output=0.06*un
     outpu=un+output
     I+=1
@@ -11,3 +11,6 @@ while True:
     print(str(output)+"\n")
     print(str(outpu)+"\n")
     un=outpu
+    if I>68:
+        break
+print("Thank You For Using Morgan's Tax Simulator")
