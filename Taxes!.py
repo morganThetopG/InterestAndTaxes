5e58a09b9d52dd622f0615ea95f788205a2ef583
morganThetopG <117848844+morganThetopG@users.noreply.github.com>
2022-11-17 09:20:40 -0700

    Add files via upload

---
+++ Taxes!.py
@@ -0,0 +1,14 @@
+while True:
+    un=float(input("enter a number: "))
+    print("\n The Number You Entered Will Enter The Intrest Simulator \n")
+    print("First Year")
+    output=0.06*un
+    outpu=un+output
+
+    outdos=0.06*outpu
+    outsos=outdos+outpu
+    print(str(output)+"\n")
+    print(str(outpu)+"\n")
+    print("Second Year")
+    print(str(outdos)+"\n")
+    print(str(outsos)+"\n")
